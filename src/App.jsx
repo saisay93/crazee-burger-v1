3ea0c75d148d92cfe0afdcddeffd7ed558b917c5
saisay93 <saisay@hotmail.com>
2025-10-08 01:42:30 +0200

    ✏️ F02: APP > fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import ErrorPage from "./components/pages/error/ErrorPage";
 import LoginPage from "./components/pages/login/LoginPage";
+import OrderPage from "./components/pages/order/OrderPage";
 
 function App() {
 	return (
-		<>
-			<LoginPage />
-		</>
+		<Routes>
+			<Route path="/" element={<LoginPage />} />
+			<Route path="/order/:name" element={<OrderPage/>} />
+			<Route path="*" element={<ErrorPage />} />
+		</Routes>
 	);
 }
 
