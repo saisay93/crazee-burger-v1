de35ed9360710e24399bc65f071b370581310895
saisay93 <saisay@hotmail.com>
2025-10-08 00:57:10 +0200

    ✅ F02: ERROR > Create <ErrorPage />

---
+++ src/components/pages/error/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+function ErrorPage() {
+	return (
+		<> 
+			<h1>ErrorPage</h1>
+			<NavLink to="/"><button type="button">Retourner à la page d'accueil</button></NavLink>
+		</>
+	);
+}
+
+export default ErrorPage;
