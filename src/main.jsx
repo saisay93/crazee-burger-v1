776c3eefe896f73740b90a029f542a5989b11bd2
saisay93 <saisay@hotmail.com>
2025-10-08 01:37:40 +0200

    ♻️ F02: SRC > Main > Refacto routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,14 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import OrderPage from "./components/pages/order/OrderPage.jsx";
-import ErrorPage from "./components/pages/error/ErrorPage.jsx";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
 	<StrictMode>
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<App />} />
-				<Route path="/order/:name" element={<OrderPage />} />
-				<Route path="/*" element={<ErrorPage />} />
-			</Routes>
+			<App />
 		</BrowserRouter>
 	</StrictMode>,
 );
