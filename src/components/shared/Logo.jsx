4d55fa2655b5a28a4c9862b0318947ee1b84d263
saisay93 <saisay@hotmail.com>
2025-10-09 02:37:04 +0200

    💄 F03: SHARED > Logo > Add style

--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -1,5 +1,47 @@
+import styled from "styled-components";
+import { theme } from "../../styles/theme";
+
 function Logo() {
-	return <div>Logo</div>;
+	return (
+		<LogoStyled>
+			<h1>
+				CRAZEE
+				<span>
+					<img
+						src="/public/images/logo-orange.png"
+						alt="logo crazee burger orange"
+					/>
+				</span>{" "}
+				BURGER
+			</h1>
+		</LogoStyled>
+	);
 }
 
 export default Logo;
+
+const LogoStyled = styled.div`
+
+  h1{
+	display: flex;
+	font-family: "Amatic SC", cursive;
+    font-size: ${theme.fonts.P4};
+    font-weight: ${theme.weights.bold};
+	line-height: 1em;
+	letter-spacing: 1.5px;
+	text-align: center;
+	align-items: center;
+	text-transform: uppercase;
+	color: ${theme.colors.primary};
+	}
+
+  img{
+	object-fit: contain;
+	object-position: center;
+	width: 80px;
+	height: 60px;
+	margin: 0 5px;
+  }
+
+
+`;
