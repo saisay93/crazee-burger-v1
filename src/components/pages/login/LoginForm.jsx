73fe26571eb7a216e465da31efeaa1b314cef099
saisay93 <saisay@hotmail.com>
2025-10-28 02:05:55 +0100

    💄F03: LOGIN > LoginForm > Add style

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import styled from "styled-components";
+import { theme } from "../../../styles/theme";
+import InputText from "../../shared/InputText";
+import { BsPersonCircle } from "react-icons/bs";
+import PrimaryButton from "../../shared/PrimaryButton";
+import { FaChevronRight } from "react-icons/fa";
 function LoginForm() {
 	const [inputValue, setInputValue] = useState("");
 	const navigate = useNavigate();
@@ -16,20 +21,52 @@ function LoginForm() {
 	};
 
 	return (
-		<form action="submit" onSubmit={handleSubmit}>
+		<LoginFormStyled action="submit" onSubmit={handleSubmit}>
 			<h1>Bienvenue chez nous !</h1>
-			<br />
+			<hr />
 			<h2>Connectez-vous</h2>
-			<input
-				type="text"
-				placeholder="Entrez votre prénom"
-				onChange={handelChange}
-				required
+			<InputText
 				value={inputValue}
+				onChange={handelChange}
+				placeholder="Entrez votre prénom"
+				Icon={<BsPersonCircle className="user-icon" />}
+				required
 			/>
-			<button type="submit">Accéder à mon espace</button>
-		</form>
+			<PrimaryButton
+				label="Accéder à mon espace"
+				Icon={<FaChevronRight className="user-icon" />}
+			/>
+		</LoginFormStyled>
 	);
 }
 
 export default LoginForm;
+
+const LoginFormStyled = styled.form`
+  max-width: 500px;
+  min-width: 40px;
+  padding: 2rem 2.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: Amatic SC, cursive;
+
+  h1{
+	font-size: ${theme.fonts.P5};
+	color: ${theme.colors.white};
+	margin-top: 2rem;
+  }
+
+  h2{
+	font-size: ${theme.fonts.P4};
+	color: ${theme.colors.white};
+  }
+
+  hr{
+	width: 400px;
+	border: 1.5px solid ${theme.colors.primary_burger};
+	margin: 2rem 0 2.5rem;
+
+  }
+`;
