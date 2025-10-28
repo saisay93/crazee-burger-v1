7bd17a0aae7f272feaff6efe62545ac146a48a51
saisay93 <saisay@hotmail.com>
2025-10-28 01:56:35 +0100

    ♻️ F03: SHARED > Logo > Refacto

--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -1,19 +1,15 @@
 import styled from "styled-components";
 import { theme } from "../../styles/theme";
 
-function Logo() {
+function Logo({ className }) {
 	return (
-		<LogoStyled>
-			<h1>
-				CRAZEE
-				<span>
-					<img
-						src="/public/images/logo-orange.png"
-						alt="logo crazee burger orange"
-					/>
-				</span>{" "}
-				BURGER
-			</h1>
+		<LogoStyled className={className}>
+			<h2>CRAZEE</h2>
+			<img
+				src="/public/images/logo-orange.png"
+				alt="logo crazee burger orange"
+			/>
+			<h2>BURGER</h2>
 		</LogoStyled>
 	);
 }
@@ -21,8 +17,11 @@ function Logo() {
 export default Logo;
 
 const LogoStyled = styled.div`
+	display: flex;
+	align-items: center;
+	
 
-  h1{
+  h2{
 	display: flex;
 	font-family: "Amatic SC", cursive;
     font-size: ${theme.fonts.P4};
