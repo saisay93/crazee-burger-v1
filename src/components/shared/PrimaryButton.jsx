c46129153d37d864f809a781928227dc8d9ef540
saisay93 <saisay@hotmail.com>
2025-10-29 05:02:13 +0100

    💄 F03: LOGIN > PrimaryButton > Update CSS

--- src/components/shared/PrimaryButton.jsx
+++ src/components/shared/PrimaryButton.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { theme } from "../../styles/theme";
+import { theme } from "../../theme";
 
 function PrimaryButton({ label, Icon }) {
 	return (
@@ -13,35 +13,29 @@ function PrimaryButton({ label, Icon }) {
 export default PrimaryButton;
 
 const PrimaryButtonStyled = styled.button`
-  background-color: ${theme.colors.primary_burger};
+  background-color: ${theme.colors.primary};
   border-radius: ${theme.borderRadius.round};
   display: inline-flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   padding: 18px 24px;
-  border: 1px solid ${theme.colors.primary_burger};
+  border: 1px solid ${theme.colors.primary};
   color: ${theme.colors.white};
-  font-size: ${theme.fonts.P0};
-  font-weight: ${theme.weights.bold};
-  
+  font-size: ${theme.fonts.size.P0};
+  font-weight: ${theme.fonts.weights.bold};
 
-  .user-icon{
-    width: 15px;
-	height: 15px;
-    margin-left: 10px;
-  }
-
-  &:hover{ 
+  &:hover {
     cursor: pointer;
     background-color: ${theme.colors.white};
-    color: ${theme.colors.primary_burger};
-    border: 1px solid ${theme.colors.primary_burger};
-   }
+    color: ${theme.colors.primary};
+    border: 1px solid ${theme.colors.primary};
+    transition: all 0.3s ease-out;
+  }
 
-   &:active{
-    background-color: ${theme.colors.primary_burger};
+  &:active {
+    background-color: ${theme.colors.primary};
     color: ${theme.colors.white};
-    border: 1px solid ${theme.colors.primary_burger};
-   }
+    border: 1px solid ${theme.colors.primary};
+  }
 `;
