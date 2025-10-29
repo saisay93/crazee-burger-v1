531733984d6df41b61c2b66a7c2a6e104df9b44c
saisay93 <saisay@hotmail.com>
2025-10-29 04:58:32 +0100

    💄 F03: LOGIN > LoginForm > Update CSS

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { theme } from "../../../styles/theme";
+import { theme } from "../../../theme";
 import InputText from "../../shared/InputText";
 import { BsPersonCircle } from "react-icons/bs";
 import PrimaryButton from "../../shared/PrimaryButton";
-import { IoChevronForward } from 'react-icons/io5';
+import { IoChevronForward } from "react-icons/io5";
 function LoginForm() {
 	const [inputValue, setInputValue] = useState("");
 	const navigate = useNavigate();
@@ -44,30 +44,27 @@ export default LoginForm;
 
 const LoginFormStyled = styled.form`
   padding: 2.5rem 2rem;
-  text-align: center; 
+  text-align: center;
   font-family: Amatic SC, cursive;
 
-  h1{
-	font-size: ${theme.fonts.size.P5};
-	color: ${theme.colors.white};
-	margin-top: 2rem;
+  h1 {
+    font-size: ${theme.fonts.size.P5};
+    color: ${theme.colors.white};
+    margin-top: 2rem;
   }
 
-  h2{
-	font-size: ${theme.fonts.size.P4};
-	color: ${theme.colors.white};
+  h2 {
+    font-size: ${theme.fonts.size.P4};
+    color: ${theme.colors.white};
   }
 
-  hr{
-	width: 400px;
-	border: 1.5px solid #F56a2c;
-	margin: 2rem 0 2.5rem;
-
+  hr {
+    width: 400px;
+    border: 1.5px solid #f56a2c;
+    margin: 2rem 0 2.5rem;
   }
 
-  .chevron{
-    width: 15px;
-	height: 15px;
+  .chevron {
     margin-left: 10px;
   }
 `;
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -6,7 +6,7 @@ import backgroung from "/public/images/burger-background.jpg";
 function LoginPage() {
 	return (
 		<LoginPageStyled>
-			<Logo className="logo-login-page"/>
+			<Logo className="logo-login-page" />
 			<LoginForm />
 		</LoginPageStyled>
 	);
@@ -20,12 +20,12 @@ const LoginPageStyled = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${backgroung});
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url(${backgroung});
   background-size: cover;
   background-position: center;
 
-  .logo-login-page{
+  .logo-login-page {
     transform: scale(2.5);
   }
-
 `;
