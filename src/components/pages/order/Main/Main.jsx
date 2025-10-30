66fa138465368323c36321f3af0d81bdf5625732
saisay93 <saisay@hotmail.com>
2025-10-30 07:42:34 +0100

    💄 F04: ORDER > Main > Add style

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -11,8 +11,9 @@ export default Main
 
 const MainStyled = styled.div`
   width: 100%;
-  height: 88vh;
+  height: 83vh;
   background: #f5f5f7;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   border-radius: 0px 0px 15px 15px;
+  margin: 0 auto;
 `;
