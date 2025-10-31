736861368905727c28a420083b092983940034e1
saisay93 <saisay@hotmail.com>
2025-10-31 03:09:28 +0100

    ♻️ F04: ORDER > Main > NavBar refacto

--- src/components/pages/order/NavBar/NavBar.jsx
+++ src/components/pages/order/NavBar/NavBar.jsx
@@ -1,83 +1,33 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../../../shared/Logo";
-import { BsPersonCircle } from "react-icons/bs";
+import { theme } from "../../../../theme/index";
+import NavBarRightSide from "./NavBarRightSide";
+import {refreshPage} from "../../../../utils/window";
 
 function NavBar() {
 	const { username } = useParams();
+
 	return (
 		<NavBarStyled>
-			<button type="button" className="logobtn">
-			  <Logo />
-			</button>
-			<div className="navBarRight">
-				<div className="greeting">
-          <h1>Hey, <span>{username}</span></h1>
-          <Link to="/">
-            <button type="button">Se déconnecter</button>
-          </Link>
-        </div>
-        <BsPersonCircle className="user-icon" />
-			</div>
+			<Logo className="logo-order-page" onClick={refreshPage}/>
+			<NavBarRightSide username={username} />
 		</NavBarStyled>
 	);
 }
 export default NavBar;
 
-const NavBarStyled = styled.div`
-  background-color: white;
-  width: 100%;
-  height: 98px;
-  border-radius: 15px 15px 0px 0px;
-  padding: 0 70px 0px 20px;
+const NavBarStyled = styled.nav`
+  background-color: ${theme.colors.white};
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  height: 10vh;
+  border-radius: ${theme.borderRadius.extraRound}
+    ${theme.borderRadius.extraRound} 0 0;
+  padding: 0 70px 0px 20px;
 
-  .logobtn:hover{
-      text-decoration: none;
-      cursor: pointer;
+  .logo-order-page:hover {
+    cursor: pointer;
   }
-
-  .navBarRight {
-    display: flex;
-    align-items: center;
-    gap: 10px;
-  }
-
-   h1 {
-    font-family: "Open Sans", sans-serif;
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 22px;
-    color: rgba(116, 123, 145, 1);
-   }
-
-  span {
-      font-family: "Open Sans", sans-serif;
-      font-weight: bold;
-      line-height: 22px;
-      color: #FF9F1A;
-    }
-
-   button {
-    border: none;
-    background-color: transparent;
-    color: rgba(116, 123, 145, 1);
-    font-family: "Open Sans", sans-serif;
-    font-size: 10px;
-    font-weight: 400;
-    line-height: 14px;
-
-    &:hover {
-      text-decoration: underline;
-      cursor: pointer;
-    }
-  }
-
-  .user-icon{
-    width: 36px;
-    height: 36px;
-    color: rgba(116, 123, 145, 1);
-  }
-  
 `;
