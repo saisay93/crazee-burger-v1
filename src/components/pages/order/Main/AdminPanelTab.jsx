1082939ac73609885d85ca7a177e76191ca3ea7e
saisay93 <saisay@hotmail.com>
2025-11-10 08:43:32 +0100

    💄 F07: ORDER > MAIN > Add style to <AdminPanelTab />

--- src/components/pages/order/Main/AdminPanelTab.jsx
+++ src/components/pages/order/Main/AdminPanelTab.jsx
@@ -1,14 +1,39 @@
 import styled from "styled-components";
+import { theme } from "../../../../theme";
 
-function AdminPanelTab() {
-	return <AdminPanelTabstyled>^</AdminPanelTabstyled>;
+function AdminPanelTab({ label, icon }) {
+	return (
+		<AdminPanelTabstyled>
+			{icon && icon}
+			{label}
+		</AdminPanelTabstyled>
+	);
 }
 
 export default AdminPanelTab;
 
-const AdminPanelTabstyled = styled.button`
-  background: #dc1d0cf0;
-  width: 40px;
-  height: 40px;
-  border: none;
+const AdminPanelTabstyled = styled.div`
+  background: ${theme.colors.white};
+  min-width: 60px;
+  height: 43px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+ 
+  border: 1px solid ${theme.colors.greyLight};
+  border-bottom: 2px solid ${theme.colors.greyLight};
+  border-radius: ${theme.borderRadius.round} ${theme.borderRadius.round} 0 0;
+
+  padding: 13px 22px;
+  margin-right: 1px;
+  font-size: ${theme.fonts.size.P0};
+  
+  color: ${theme.colors.greySemiDark};
+  cursor: pointer;
+  
+  &:hover {
+    text-decoration: underline;
+    border-bottom: 2px solid ${theme.colors.white};
+
+  }
 `;
